51b2b83622305e87a6d2f5a0c86ce1dec5e3288d
motorro <motorro@gmail.com>
2021-05-08 18:22:58 +0300

    Test for container

--- app/src/test/java/com/motorro/parcellerfail/FailingClassTest.kt
+++ app/src/test/java/com/motorro/parcellerfail/FailingClassTest.kt
@@ -1,7 +1,9 @@
 package com.motorro.parcellerfail
 
 import android.os.Build
+import android.os.Bundle
 import android.os.Parcel
+import org.junit.Assert
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -10,18 +12,13 @@ import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
 @Config(sdk = [Build.VERSION_CODES.P])
-class PairParcelerTest {
+class FailingClassTest {
     @Test
     fun parcels() {
-        val interval = Pair(10, 20)
-        val parcel = Parcel.obtain()
-        val parceler = PairParceler(IntParceler, IntParceler)
-
-        with (parceler) {
-            interval.write(parcel, 0)
-        }
-        parcel.setDataPosition(0)
-
-        assertEquals(interval, parceler.create(parcel))
+        val failing = FailingClass(Pair(10, 20))
+        val bundle = Bundle()
+        bundle.putParcelable("test", failing)
+        val fromParcel = bundle.deepCopy().getParcelable<FailingClass>("test")
+        assertEquals(failing, fromParcel)
     }
 }
